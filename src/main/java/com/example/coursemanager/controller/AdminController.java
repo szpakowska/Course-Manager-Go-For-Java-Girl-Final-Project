968ecb75d329a6c61e37acb03225fe78754e15a5
anna-szpakowska <annaszpakowska@outlook.com>
2023-11-30 18:37:49 +0100

    Added endpoints, controllers and corresponding htmls

--- src/main/java/com/example/coursemanager/controller/AdminController.java
+++ src/main/java/com/example/coursemanager/controller/AdminController.java
@@ -1,40 +1,75 @@
 package com.example.coursemanager.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
-    @GetMapping("/registration")
-    public String showRegistrationPage(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            String username = authentication.getName();
-            model.addAttribute("username", username);
+    @GetMapping("/user-addition")
+    public String showUserAdditionPage() {
+        return "admin/UserAddition";}
 
 
-            Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-            List<String> roles = new ArrayList<>();
-            for (GrantedAuthority authority : authorities) {
-                roles.add(authority.getAuthority());
-            }
-            model.addAttribute("roles", roles);
+    @GetMapping("/user-deletion")
+    public String showUserDeletionPage() {
+        return "admin/UserDeletion";}
 
-        }
-        return "Registration";
+
+    @GetMapping("/user-edition")
+    public String showUserEditionPage() {
+        return "admin/UserEdition";}
+
+    @GetMapping("/report-of-users")
+    public String showReportOfUserPage() {
+        return "admin/ReportOfUsers";}
+
+    @GetMapping("/report-of-courses")
+    public String showReportOfCoursesPage() {
+        return "admin/ReportOfCourses";}
+
+    @GetMapping("/report-of-blocks")
+    public String showReportOfBlocksPage() {
+        return "admin/ReportOfBlocks";}
+
+    @GetMapping("/course-edition")
+    public String showCourseEditionPage() {
+        return "admin/CourseEdition";}
+
+    @GetMapping("/course-deletion")
+    public String showCourseDeletionPage() {
+        return "admin/CourseDeletion";}
+
+    @GetMapping("/course-addition")
+    public String showCourseAdditionPage() {
+        return "admin/CourseAddition";}
+
+    @GetMapping("/block-edition")
+    public String showBlockEditionPage() {
+        return "admin/BlockEdition";}
+
+    @GetMapping("/block-deletion")
+    public String showBlockDeletionPage() {
+        return "admin/BlockDeletion";}
+
+    @GetMapping("/block-addition")
+    public String showBlockAdditionPage() {
+        return "admin/BlockAddition";}
+
+
+
+
+
+
+
+
 
 
     }
-}
+
+
+
+
 
 
