ce1a3ada468df38d87a48eb149cffc5604081366
MonikaMieleszko <kawka.monika.89@gmail.com>
2023-12-04 23:16:51 +0100

    Added CRUD methods for User Entity.

--- src/main/java/com/example/coursemanager/utils/mapper/UserMapper.java
+++ src/main/java/com/example/coursemanager/utils/mapper/UserMapper.java
@@ -1,4 +1,31 @@
 package com.example.coursemanager.utils.mapper;
 
+import com.example.coursemanager.dto.UserDto;
+import com.example.coursemanager.model.User;
+
 public class UserMapper {
+    public static User toUserModel(UserDto userDto) {
+        User user = new User();
+        user.setUsername(userDto.getUsername());
+        user.setPassword(userDto.getPassword());
+        user.setEmail(userDto.getEmail());
+        user.setName(userDto.getName());
+        user.setSurname(userDto.getSurname());
+        user.setActive(userDto.getActive());
+        user.setRoles(userDto.getRoles());
+        return user;
+    }
+
+    public static UserDto toUserDto(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setUsername(user.getUsername());
+        userDto.setPassword(user.getPassword());
+        userDto.setEmail(user.getEmail());
+        userDto.setName(user.getName());
+        userDto.setSurname(user.getSurname());
+        userDto.setActive(user.getActive());
+        userDto.setRoles(user.getRoles());
+        return userDto;
+    }
+
 }
