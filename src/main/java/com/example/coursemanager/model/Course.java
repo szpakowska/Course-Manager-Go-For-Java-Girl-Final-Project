6606b716300250b568ea2ebdbd05edcd56e8db1f
anna-szpakowska <annaszpakowska@outlook.com>
2023-12-11 00:57:17 +0100

    Join table fix

--- src/main/java/com/example/coursemanager/model/Course.java
+++ src/main/java/com/example/coursemanager/model/Course.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.Set;
 
 @Data
@@ -19,13 +20,13 @@ public class Course {
         this.name = name;
     }
 
-    @ManyToMany
+    @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(
             name = "course_students",
             joinColumns = { @JoinColumn(name = "course_id") },
             inverseJoinColumns = { @JoinColumn(name = "students_id") }
     )
-    private Set<User> students;
+    private List<User> students;
 
     @OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
     private Set<Block> blocks;
