786e447d00caab5a841c5aa54552141c5c17ace4
anna-szpakowska <annaszpakowska@outlook.com>
2023-12-13 19:50:45 +0100

    lesson edition - in progress

--- src/main/java/com/example/coursemanager/service/LessonService.java
+++ src/main/java/com/example/coursemanager/service/LessonService.java
@@ -1,10 +1,12 @@
 package com.example.coursemanager.service;
 
+import com.example.coursemanager.dto.LessonDto;
 import com.example.coursemanager.model.Lesson;
 import com.example.coursemanager.repository.LessonJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.List;
 
 @Service
@@ -16,9 +18,22 @@ public class LessonService {
         lessonJpaRepository.deleteById(id);
     }
 
-    public List<Lesson> getAllLessons() {return lessonJpaRepository.findAll();
+    public List<Lesson> getAllLessons() {
+        return lessonJpaRepository.findAll();
     }
 
-    public Lesson getLessonById(Long id) {return lessonJpaRepository.findById(id).orElseThrow(RuntimeException::new);
+    public Lesson getLessonById(Long id) {
+        return lessonJpaRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    public void updateLesson(LessonDto lessonDto, Long id) {
+        Lesson lesson = lessonJpaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Lesson not found"));
+
+        lesson.setSubject(lessonDto.subject());
+        lesson.setLocalDateTime(lessonDto.localDateTime());
+        lesson.setDuration(Duration.ofMinutes(lessonDto.durationInMinutes()));
+
+        lessonJpaRepository.save(lesson);
     }
 }
